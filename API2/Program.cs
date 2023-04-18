a444ebffe98229fb50bef38afb3d995cc92a6143
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-04-17 21:49:31 -0300

    First commit

---
+++ API2/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/api/v1/hello", () => "-> 2nd API MOTD: Hey, I am #2 API >:P").WithName("GetHelloWorld");
+
+app.Run();
